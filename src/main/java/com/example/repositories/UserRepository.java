3a8859e1fae317161d7945fff147b878b3dcdb84
David Zambrano <zambranodavid1014@gmail.com>
2024-02-09 11:25:06 -0500

    base project roles

---
+++ src/main/java/com/example/repositories/UserRepository.java
@@ -0,0 +1,20 @@
+package com.example.repositories;
+
+import com.example.models.UserEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity,Long> {
+    //with JPA
+    Optional<UserEntity> findByUsername(String userName);
+
+    //with Query
+    @Query("select u from UserEntity u where u.username =?1")
+    Optional<UserEntity> getName(String userName);
+
+
+}
